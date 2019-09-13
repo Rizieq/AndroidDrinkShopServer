267f7451e289f502ce495eb87ff8f5a1f5a0d9a4
rizieq <rizieqgifari13012003@gmail.com>
2019-09-13 15:39:48 +0700

    Add Notification Firebase

--- app/src/main/java/com/rizieq/androiddrinkshopserver/Services/MyFirebaseMessaging.java
+++ app/src/main/java/com/rizieq/androiddrinkshopserver/Services/MyFirebaseMessaging.java
@@ -1,12 +1,69 @@
 package com.rizieq.androiddrinkshopserver.Services;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.rizieq.androiddrinkshopserver.R;
+import com.rizieq.androiddrinkshopserver.utils.NotificationHelper;
+
+import java.util.Map;
+import java.util.Random;
 
 public class MyFirebaseMessaging extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+
+        if (remoteMessage.getData() != null)
+        {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                sendNotificationAPI26(remoteMessage);
+            else
+                sendNotification(remoteMessage);
+        }
+    }
+
+    private void sendNotification(RemoteMessage remoteMessage) {
+        // GET Information from Message
+        Map<String,String> data = remoteMessage.getData();
+        String title = data.get("title");
+        String message = data.get("message");
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri);
+
+        NotificationManager noti = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        noti.notify(new Random().nextInt(),builder.build());
+    }
+
+    private void sendNotificationAPI26(RemoteMessage remoteMessage) {
+
+        // GET Information from Message
+        Map<String,String> data = remoteMessage.getData();
+        String title = data.get("title");
+        String message = data.get("message");
+        // FROM API level 26 , we need implement Notification Channel
+        NotificationHelper helper;
+        Notification.Builder builder;
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        helper = new NotificationHelper(this);
+        builder = helper.getDrinkShopNotification(title,message,defaultSoundUri);
+
+        helper.getManager().notify(new Random().nextInt(),builder.build());
+
     }
 }
