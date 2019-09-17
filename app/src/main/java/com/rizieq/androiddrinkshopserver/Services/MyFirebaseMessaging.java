7f4c46c6884e38c1165793baf77745025e9b9a03
rizieq <rizieqgifari13012003@gmail.com>
2019-09-17 13:16:46 +0700

    Update intent Notification

--- app/src/main/java/com/rizieq/androiddrinkshopserver/Services/MyFirebaseMessaging.java
+++ app/src/main/java/com/rizieq/androiddrinkshopserver/Services/MyFirebaseMessaging.java
@@ -2,7 +2,9 @@ package com.rizieq.androiddrinkshopserver.Services;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
@@ -13,6 +15,7 @@ import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.rizieq.androiddrinkshopserver.R;
+import com.rizieq.androiddrinkshopserver.ShowOrderActivity;
 import com.rizieq.androiddrinkshopserver.retrofit.IDrinkshopAPI;
 import com.rizieq.androiddrinkshopserver.utils.Common;
 import com.rizieq.androiddrinkshopserver.utils.NotificationHelper;
@@ -63,10 +66,16 @@ public class MyFirebaseMessaging extends FirebaseMessagingService {
     }
 
     private void sendNotification(RemoteMessage remoteMessage) {
+
         // GET Information from Message
+        Intent intent = new Intent(getBaseContext(), ShowOrderActivity.class);
+        PendingIntent contextIntent = PendingIntent.getActivity(getBaseContext(),0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+
         Map<String,String> data = remoteMessage.getData();
         String title = data.get("title");
         String message = data.get("message");
+
+
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
@@ -74,6 +83,7 @@ public class MyFirebaseMessaging extends FirebaseMessagingService {
                 .setContentTitle(title)
                 .setContentText(message)
                 .setAutoCancel(true)
+                .setContentIntent(contextIntent)
                 .setSound(defaultSoundUri);
 
         NotificationManager noti = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
@@ -81,6 +91,8 @@ public class MyFirebaseMessaging extends FirebaseMessagingService {
     }
 
     private void sendNotificationAPI26(RemoteMessage remoteMessage) {
+
+
 
         // GET Information from Message
         Map<String,String> data = remoteMessage.getData();
